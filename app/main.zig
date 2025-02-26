d42306b68fab1fdb2bc9e6c80b93aea2b1b1869d
Varun Madiath <github@madiathv.com>
2025-02-26 08:11:19 -0500

    WIP

--- app/main.zig
+++ app/main.zig
@@ -6,25 +6,29 @@ const BencodedResult = struct {
     const Payload = union(enum) {
         string: []const u8,
         int: i64,
+        list: []const BencodedResult,
     };
 
     payload: Payload,
     bytes_read: usize,
 };
 
-
+const BencodeError = error{
+    InvalidArgument,
+    OutOfMemory, // needed for ArrayList operations
+};
 
 pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
-    
+
     if (args.len < 3) {
         try stdout.print("Usage: your_bittorrent.zig <command> <args>\n", .{});
         std.process.exit(1);
     }
 
     const command = args[1];
-        
+
     if (std.mem.eql(u8, command, "decode")) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         // try stdout.print("Logs from your program will appear here\n", .{});
@@ -39,6 +43,7 @@ pub fn main() !void {
         switch (decodedStr.payload) {
             inline .int,
             .string,
+            .list,
             => |payload| try std.json.stringify(payload, .{}, string.writer()),
         }
 
@@ -47,12 +52,12 @@ pub fn main() !void {
     }
 }
 
-fn parseInt(encodedValue: []const u8, startIdx: usize) !BencodedResult {
+fn parseInt(encodedValue: []const u8, startIdx: usize) BencodeError!BencodedResult {
     var result: i64 = 0;
     var idx = startIdx;
     var factor: i64 = 1;
 
-    if(encodedValue[idx] == '-') {
+    if (encodedValue[idx] == '-') {
         factor = -1;
         idx += 1;
     }
@@ -65,15 +70,15 @@ fn parseInt(encodedValue: []const u8, startIdx: usize) !BencodedResult {
 
     idx += 1;
 
-    return BencodedResult {
+    return BencodedResult{
         .payload = .{
             .int = result * factor,
         },
-        .bytes_read = idx,
+        .bytes_read = idx - startIdx,
     };
 }
 
-fn decodeBencode(encodedValue: []const u8) !BencodedResult {
+fn decodeBencode(encodedValue: []const u8) BencodeError!BencodedResult {
     var idx: usize = 0;
 
     if (encodedValue[idx] >= '0' and encodedValue[idx] <= '9') {
@@ -90,9 +95,37 @@ fn decodeBencode(encodedValue: []const u8) !BencodedResult {
     } else if (encodedValue[idx] == 'i') {
         idx += 1;
         return parseInt(encodedValue, idx);
+    } else if (encodedValue[idx] == 'l') {
+        idx += 1;
+        return decodeList(encodedValue, idx);
     }
+    // else if (encodedValue[idx] == 'd') {
+    //     idx += 1;
+    //     return decodeDict(encodedValue, idx);
+    // }
     else {
-        try stdout.print("Only strings are supported at the moment\n", .{});
-        std.process.exit(1);
+        return error.InvalidArgument;
     }
+}
+
+// fn decodeDict(encodedValue: []const u8, startIdx: usize) !BencodedResult {
+//     var idx = startIdx;
+//     var dict = std.HashMap(BencodedResult, BencodedResult, std.hash.SipHash, 10).init(allocator);
+// }
+
+fn decodeList(encodedValue: []const u8, startIdx: usize) BencodeError!BencodedResult {
+    var idx = startIdx;
+    var list = std.ArrayList(BencodedResult).init(allocator);
+    errdefer list.deinit();
+
+    while (encodedValue[idx] != 'e') {
+        const decoded = try decodeBencode(encodedValue[idx..]);
+        try list.append(decoded);
+        idx += decoded.bytes_read;
+    }
+
+    return BencodedResult{
+        .payload = .{ .list = try list.toOwnedSlice() },
+        .bytes_read = idx - startIdx,
+    };
 }
